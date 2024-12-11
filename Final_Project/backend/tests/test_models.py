4acaad84f673e4b260d4bef9fec5b93cd7a82452
ealvare04 <157135571+ealvare04@users.noreply.github.com>
2024-12-10 21:34:14 -0500

    updating test_models

--- Final_Project/backend/tests/test_models.py
+++ Final_Project/backend/tests/test_models.py
@@ -39,3 +39,26 @@ def test_user_model_creation(client):
         # Assert the default values for amount and currency are set correctly
         assert created_user.amount == 0.0  # Default value for amount
         assert created_user.currency == "USD"  # Default value for currency
+
+def test_user_model_creation_specified(client):
+    # Create a new user instance specifying amount or currency
+    user = User(username="testuser", salt="test_salt", hashed_password="test_hashed_password", amount=20.0, currency = "CAD")
+    
+    # Ensure the app context is available when interacting with the database
+    with app.app_context():
+        # Add user to the session and commit it to the database
+        db.session.add(user)
+        db.session.commit()
+        
+        # Retrieve the user from the database
+        created_user = User.query.filter_by(username="testuser").first()
+        
+        # Check the user properties
+        assert created_user.username == "testuser"
+        assert created_user.salt == "test_salt"
+        assert created_user.hashed_password == "test_hashed_password"
+        
+        # Assert the default values for amount and currency are set correctly
+        assert created_user.amount == 20.0  # Specified value for amount
+        assert created_user.currency == "CAD"  # Specified value for currency
+
